636f3af4229ebe26eaa14063454cfc0faf3fe3c7
Jyothis <jyothis.hiworth@gmail.com>
2025-06-01 22:11:48 +0530

    lead and contact table done

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "959035943"
+    serviceWorkerVersion: "3839206436"
   }
 });
