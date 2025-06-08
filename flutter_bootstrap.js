6e987929dda87e305c4bbb6b44f05d073fffe04c
Jyothis <jyothis.hiworth@gmail.com>
2025-06-08 22:17:23 +0530

    bugs fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2630366153"
+    serviceWorkerVersion: "1554616133"
   }
 });
