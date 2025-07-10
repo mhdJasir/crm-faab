c93799826f652a964926ebeaca8f34fd306db6e5
Jyothis <jyothis.hiworth@gmail.com>
2025-07-10 08:57:33 +0530

    roc inprogress

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2348150342"
+    serviceWorkerVersion: "3418479420"
   }
 });
