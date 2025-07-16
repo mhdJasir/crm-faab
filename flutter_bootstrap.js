1d41fec3251a3938f2f23b5ae07c43832bb2fee0
Jyothis <jyothis.hiworth@gmail.com>
2025-07-16 09:21:03 +0530

    my push

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3611670699"
+    serviceWorkerVersion: "2682760475"
   }
 });
